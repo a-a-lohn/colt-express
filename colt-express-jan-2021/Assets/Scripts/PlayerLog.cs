a66edab58165eea5cf04fcd25b9afc0e4a78fbe8
Insha Sohail <syedahinsha@gmail.com>
2021-03-01 19:21:51 -0500

    game log script

--- colt-express-jan-2021/Assets/Scripts/PlayerLog.cs
+++ colt-express-jan-2021/Assets/Scripts/PlayerLog.cs
@@ -1,39 +1,58 @@
- using UnityEngine;
- using System.Collections;
- using System.Collections.Generic;
- 
- public class PlayerLog : MonoBehaviour 
- {
- public int maxLines = 5;
- 
- private Queue queue = new Queue();
- private string[] testStrings = {"Eating crackers", "Stomping foot", "Trying lock", "Drinking potion", "Picking up dagger", "Hitting the wall", "Turning off lights"};
- private string text = "";
- 
- void Start() {
-     InvokeRepeating("Test", 0, 2);
- }
- 
- void Test() {
-     NewActivity(testStrings[Random.Range(0,testStrings.Length)]);
- }
- 
- public void NewActivity(string activity) {
-     if (queue.Count >= 5)
-         queue.Dequeue();
+using UnityEngine;
+ using UnityEngine.EventSystems;
+ using System.Collections;
+ using System.Collections.Generic;
+ 
+ public class PlayerLog : MonoBehaviour 
+ {
+     // Private VARS
+     private List<string> Eventlog = new List<string>();
+     public string guiText = "";
+ 
+     // Public VARS
+     public int maxLines = 3;
 
-     queue.Enqueue(activity);
-     
-     text = "";
-     foreach (string st in queue)
-         text = text + st + "\n";
- }
- 
+     public void AddEvent(string eventString)
+     {
+         Eventlog.Add(eventString);
+ 
+         if (Eventlog.Count >= maxLines)Eventlog.RemoveAt(0);
+ 
+         guiText = "";
+ 
+         foreach (string logEvent in Eventlog)
+         {
+             guiText += logEvent;
+             guiText += "\n";
+        }
+     }
+ 
+    private PlayerLog eventLog;
+ 
+     void Start()
+     {
+         eventLog = GetComponent<PlayerLog>();
+     }
+     
+     void Update () 
+     {
+        string button = EventSystem.current.currentSelectedGameObject.name;
+        
+        if (button == EventSystem.current.currentSelectedGameObject.name)
+         eventLog.AddEvent("Player clicked on " + button);
 
-    void OnGUI() {
- 
-      GUI.Label(new Rect(5,(Screen.height - 150),300f,150f), text,GUI.skin.textArea);    
-         
-     }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+         eventLog.AddEvent("Player Moves Left");
+ 
+        else if (Input.GetKey(KeyCode.RightArrow))
+         eventLog.AddEvent("Player Moves Right");
 
- }
+     }
+
+
+    void OnGUI() {
+ 
+      GUI.Label(new Rect(5,(Screen.height - 150),300f,150f), guiText,GUI.skin.textArea);    
+         
+     }
+ }
