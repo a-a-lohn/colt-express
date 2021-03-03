5b4404e17445836e7455db9523b1e6619707eab9
xren935 <59517115+xren935@users.noreply.github.com>
2021-03-02 22:24:48 -0800

    Log is working!!!

--- colt-express-jan-2021/Assets/Scripts/PlayerLog.cs
+++ colt-express-jan-2021/Assets/Scripts/PlayerLog.cs
@@ -1,26 +1,92 @@
 using UnityEngine;
  using UnityEngine.EventSystems;
+using UnityEngine.UI;
  using System.Collections;
  using System.Collections.Generic;
  
  public class PlayerLog : MonoBehaviour {
      // Private VARS
-     private List<string> Eventlog = new List<string>();
-     public string guiText = "";
-     private PlayerLog eventLog;
-     
- 
+     public List<string> Eventlog = new List<string>();
+      // public string guiText = "";
+     public string guiText = "Player Played: ";
+     private string logmsg; 
+     public int maxLines = 3; 
+        public GameObject cardA; 
+        public Text log; 
+     // public Text msg; 
+//      void OnGUI() {
+//         GUI.Label(new Rect(10,(Screen.height - 150),300f,150f), guiText,GUI.skin.textArea);     
+//      }
+
+//      public void AddEvent(string eventString){
+//          Eventlog.Add(eventString);
+//  
+//          if (Eventlog.Count >= maxLines) Eventlog.RemoveAt(0);
+         
+//          guiText = ""; 
+
+//          foreach (string logEvent in Eventlog)
+//          {
+//              guiText += logEvent;
+//              guiText += "\n";
+//         }
+//      }
+
      void Start(){
-         eventLog = GetComponent<PlayerLog>();
-         eventLog.AddEvent("Player Moves Left");
-         eventLog.AddEvent("Player Moves Right");
-          string name =  EventSystem.current.currentSelectedGameObject.name;
+       // log = GetComponent<Text>(); 
+     //     eventLog.AddEvent("Player Moves Left");
+     //     eventLog.AddEvent("Player Moves Right");
+          // string name =  EventSystem.current.currentSelectedGameObject.name;
           // Debug.Log(name);
-         AddEvent("added");
+         // AddEvent("added");
+         log.text = guiText;
+ 
+ 
      }
+
+     public void OnButtonClick()
+     {
+         var go = EventSystem.current.currentSelectedGameObject;
+         if (go != null)
+             Debug.Log("Clicked on : "+ go.name);
+         else
+             Debug.Log("curr game obj is null :(");
+
+          guiText += go.name;
+          guiText += "\n";
+     }
+
+//      public void AddEvent(string eventString){
+ 
+//  
+//          // if (Eventlog.Count >= maxLines) Eventlog.RemoveAt(0);
+         
+//          guiText = ""; 
+//          guiText += eventString; 
+
+// //          foreach (string logEvent in Eventlog)
+// //          {
+// //              guiText += logEvent;
+// //              guiText += "\n";
+// //         }
+//      }
 
      void Update () 
      {
+          log.text = guiText;
+          // msg.text = guiText; 
+          // if(Input.GetButtonDown("CardA")){
+          //      Debug.Log("CARDA");
+
+          //      Ray ray = Camera.main.ScreenPointToRay( Input.mousePosition );
+          //      RaycastHit hit;
+          //      if( Physics.Raycast( ray, out hit, 100 ) )
+          //      {
+          //           Debug.Log( hit.transform.gameObject.name );
+          //      } 
+
+          // }
+     
 //         string button = EventSystem.current.currentSelectedGameObject.name;
 //         
 //         if (button == EventSystem.current.currentSelectedGameObject.name)
@@ -31,35 +97,27 @@ using UnityEngine;
 //  
 //         else if (Input.GetKey(KeyCode.RightArrow))
 //          eventLog.AddEvent("Player Moves Right");
-       string name =  EventSystem.current.currentSelectedGameObject.name;
+      // string name =  EventSystem.current.currentSelectedGameObject.name;
           // Debug.Log(name);
-        if (Input.GetKey(KeyCode.LeftArrow))
-             eventLog.AddEvent("Player Moves Left");
+          
+     //    if (Input.GetKey(KeyCode.LeftArrow))
+     //         eventLog.AddEvent("Player Moves Left");
  
-         if (Input.GetKey(KeyCode.RightArrow))
-             eventLog.AddEvent("Player Moves Right");
+     //     if (Input.GetKey(KeyCode.RightArrow))
+     //         eventLog.AddEvent("Player Moves Right");
 
      }
+
+     // void OnMouseDown(){
+     //      // if (!Input.GetMouseButtonDown(0)) return;
+     //      Debug.Log (this.gameObject.name);
+     //      eventLog.AddEvent("Player Moves Left");
+     //      eventLog.AddEvent("Player Moves Right");
+     // }
 
      // Public VARS
-     public int maxLines = 3;
+     // public int maxLines = 3;
 
-
-    void OnGUI() {
-        GUI.Label(new Rect(10,(Screen.height - 150),300f,150f), guiText,GUI.skin.textArea);     
-     }
-
-     public void AddEvent(string eventString){
-         Eventlog.Add(eventString);
- 
-         if (Eventlog.Count >= maxLines)Eventlog.RemoveAt(0);
- 
-         foreach (string logEvent in Eventlog)
-         {
-             guiText += logEvent;
-             guiText += "\n";
-        }
-     }
 
   
 
