3a8a4f6e8d7c1d47436a651fb2fbc89bd2bb0937
alohne <aaron.lohner@mail.mcgill.ca>
2021-03-22 16:09:14 -0400

    add save game functionaly with LS (not with server yet)

--- colt-express-jan-2021/Assets/Scripts/WaitingRoom.cs
+++ colt-express-jan-2021/Assets/Scripts/WaitingRoom.cs
@@ -22,13 +22,21 @@ public class WaitingRoom : MonoBehaviour
 {
     private static RestClient client = new RestClient("http://13.72.79.112:4242");
 
-    public Text fToken;
-    public Button NewGameButton;
-    //public Button GoToGameButton;
-    public Button LaunchGameButton;
     public Button HostGameButton;
+    public Button LaunchGameButton;
+    public Button NewGameButton;
+    public Button SavedSessionButtonA;
+    public Button SavedSessionButtonB;
+    public Button DeleteButton;
+
+    public Text fToken; // newgamebutton text
+    public Text SavedSessionButtonAText;
+    public Text SavedSessionButtonBText;
+    public Text SavedSessionIDButtonAText;
+    public Text SavedSessionIDButtonBText;
     public Text InfoText;
     public Text launchText;
+    public Text deleteText;
 
 
     public static string gameHash = null;
@@ -38,8 +46,11 @@ public class WaitingRoom : MonoBehaviour
     private static bool joined = false;
     public static bool hosting = false;
     public static int numPlayers;
+    public static int numSessions = 0;
+    public static bool intentToDelete = false;
 
     private static Dictionary<Button, string> hashes = new Dictionary<Button, string>();
+    private static Dictionary<Button, string> saveMap = new Dictionary<Button, string>();
 
     // Start is called before the first frame update
     void Start()
@@ -58,15 +69,50 @@ public class WaitingRoom : MonoBehaviour
             SFS.Connect(username);
         }
 
-        NewGameButton.interactable = false;
-        //GoToGameButton.interactable = false;
+        HostGameButton.interactable = false;
         LaunchGameButton.interactable = false;
-        // fToken.text = waitToken;
+        NewGameButton.interactable = false;
+        SavedSessionButtonA.interactable = false;
+        SavedSessionButtonB.interactable = false;
+        DeleteButton.interactable = false;
+
         GetSessions();
 
         //TODO:
         // make enter room functionality only occur when entered gameboard
         // do not allow the same user to log in from multiple machines
+
+
+        // add savegame logic to WR
+        /*
+        - //have >=2 buttons each for savegames
+        - //clicking hostgame launches a session
+        - clicking on a savegame creates a forked session
+        - clicking 'save' during gameplay creates a savegame. Every subsequent
+          click to save the game does nothing in LS but updates saved game on server
+        - completing/leaving a game that was never saved deletes the launched session
+        - completing a game that was saved (either during the game or earlier)
+          first deletes the savegames with that savegameid, then the session
+        - leaving a game that was saved deletes the launched session but keeps the last
+          savegame
+        - add a delete savegame button to WR that deletes savegames (and unlaunched
+          games forked from it)
+
+        - //if someone logs out, they leave the session. If they are the host, the session
+          is deleted and the buttons become clickable
+        - //only one session button is needed at all times. Once someone hosts (by clicking
+          host game or clicking on a savegame), no one else should be able to do anything
+          but join that game. When you enter the WR, either there will be a game ready
+          for you to join that has been launched by someone and you can't click anything
+          else, or you can host a game. There is never an unlaunched session going on
+          unless its about to be played
+        - change logic to require 'min-players' # of people to register before
+          a game becomes launchable, not just a fixed number like 2 or 3. Because
+          savegames can have a higher min number
+
+        - do not allow for more than 2/3 savegames to ever be created
+        - add savegame api call logic to a 'settings' scene--no savegames are created in WR
+        */
     }
 
     // Update is called once per frame
@@ -76,34 +122,21 @@ public class WaitingRoom : MonoBehaviour
 			SFS.ProcessEvents();
 		}
 
-        /*if (SessionCreated())
-        {
-            NewGameButton.SetActive(true);
-            if (NewGameButton.activeSelf)
-            {
-                if (LaunchStatus())
-                {
-                    fToken.text = "A new game has been launched";
-                }
-            }
-        }*/
-        //Invoke("GetSessions", 1);
-        if(!SFS.enteredGame) {
+        GetSessions();
+
+        if(!SFS.enteredGame && numSessions > 0) {
             if(hosting && !LaunchGameButton.interactable) {
                 ActivateLaunchGameButton();
             }
             GoToGame();
         }
-        GetSessions();
-        
+        Invoke("Stall",1);
     }
 
-    /*public void LeaveRoom() {
-        SFS.LeaveRoom();
-    }*/
+    private void Stall() { }
 
     public void GoToGame() {
-        if (gameHash != null && joined)/* && !GoToGameButton.interactable) */{
+        if (gameHash != null && joined) {
             var request = new RestRequest("api/sessions/" + gameHash, Method.GET)
                 .AddHeader("Authorization", "Basic YmdwLWNsaWVudC1uYW1lOmJncC1jbGllbnQtcHc=");
             IRestResponse response = client.Execute(request);
@@ -138,7 +171,10 @@ public class WaitingRoom : MonoBehaviour
         Dictionary<string, object> sessionDetails = obj.ToObject<Dictionary<string, object>>();
         numPlayers = 1 + sessionDetails["players"].ToString().ToCharArray().Count(c => c == ',');
 
-        if (numPlayers >= 2) { //change to >2
+        //Dictionary<string, object> sessionGameParams = (Dictionary<string, object>)sessionDetails["gameParameters"];
+        int minPlayers = 2; //number of players from savegame //(int)sessionGameParams["minSessionPlayers"];
+        // get savegame if it is one, check how many players it needs
+        if (numPlayers >= Mathf.Max(2, minPlayers)) { //change to 3
             LaunchGameButton.interactable = true;
         }
     }
@@ -153,28 +189,34 @@ public class WaitingRoom : MonoBehaviour
             entry.Key.interactable = false;
         } 
         LaunchGameButton.interactable = false;
-        HostGameButton.interactable = false;
+        //HostGameButton.interactable = false;
         joined = true;
         InfoText.text = "You will be brought to the next scene once the host launches the game!";
     }
 
-    public void CreateSession()
+    public void CreateSession(Text savegameID)
     {
-        dynamic j = new JObject();
-        j.creator = username;
-        j.game = "ColtExpress";
-        j.savegame = "";
+        if(!intentToDelete) {
+            dynamic j = new JObject();
+            j.creator = username;
+            j.game = "ColtExpress";
+            if(savegameID == null) {
+                Debug.Log("null");
+                j.savegame = "";
+            } else {
+                j.savegame = savegameID.text;
+            }
 
-        var request = new RestRequest("api/sessions?access_token=" + token, Method.POST)
-            //.AddParameter("access_token", token)
-            .AddParameter("application/json", j.ToString(), ParameterType.RequestBody)
-            .AddHeader("Authorization", "Basic YmdwLWNsaWVudC1uYW1lOmJncC1jbGllbnQtcHc=");
-        IRestResponse response = client.Execute(request);
-        gameHash = response.Content;
-        HostGameButton.interactable = false;
-        hosting = true;
-        joined = true;
-        InfoText.text = "You will be brought to the next scene once you launch your game!";
+            var request = new RestRequest("api/sessions?access_token=" + token, Method.POST)
+                .AddParameter("application/json", j.ToString(), ParameterType.RequestBody)
+                .AddHeader("Authorization", "Basic YmdwLWNsaWVudC1uYW1lOmJncC1jbGllbnQtcHc=");
+            IRestResponse response = client.Execute(request);
+            gameHash = response.Content;
+            //HostGameButton.interactable = false;
+            hosting = true;
+            joined = true;
+            InfoText.text = "You will be brought to the next scene once you launch your game!";
+        }
     }
 
     // FIX, ATTACH TO BUTTONS, ALLOW FOR MAX 3 SESSIONS
@@ -186,7 +228,7 @@ public class WaitingRoom : MonoBehaviour
         var obj = JObject.Parse(response.Content);
         Dictionary<string, Dictionary<string, Dictionary<string, object>>> sessions = obj.ToObject<Dictionary<string, Dictionary<string, Dictionary<string, object>>>>();
         //Dictionary<string, Dictionary<string, Dictionary<string, string>>> sessionsPlayers = obj.ToObject<Dictionary<string, Dictionary<string, Dictionary<string, string>>>>();
-
+        int numSessionsTemp = 0;
         foreach(KeyValuePair<string, Dictionary<string, object>> entry in sessions["sessions"])
         {
             // create a gamebutton for each game here
@@ -212,12 +254,115 @@ public class WaitingRoom : MonoBehaviour
 
 
             
-
+            numSessionsTemp++;
             // for now, we only have one button, so break after first session
-            break;
-            
+            break;   
+        }
+        numSessions = numSessionsTemp;
+
+        GetSaveGames();
+
+        if(numSessions==0) {
+            DeleteButton.interactable = true;
+            if(intentToDelete) {
+                HostGameButton.interactable = false;
+            } else {
+                HostGameButton.interactable = true;
+            }
+            if(hashes.Count > 0) {
+                hashes.Remove(NewGameButton);
+                fToken.text = "";
+                NewGameButton.interactable = false;
+                InfoText.text = "You can:\n-Host a new game\n-Click on a saved game to host it\n-Click on a game session to join it";
+            }
+        } else {
+            HostGameButton.interactable = false;
+            DeleteButton.interactable = false;
+            if(joined) {
+                NewGameButton.interactable = false;
+                SavedSessionButtonA.interactable = false;
+                SavedSessionButtonB.interactable = false;
+            }
         }
 
+    }
+
+    private void GetSaveGames() {
+        var request = new RestRequest("api/gameservices/ColtExpress/savegames?access_token=" + token, Method.GET);
+        IRestResponse response = client.Execute(request);
+
+        //var obj = JObject.Parse(response.Content);
+        var arr = JArray.Parse(response.Content);
+        //Debug.Log(arr);
+        //Debug.Log("type: " + Type.GetType(arr).ToString());
+
+        List<Dictionary<string, object>> saveGames = arr.ToObject<List<Dictionary<string, object>>>();//response.Content;
+        //List<Dictionary<string, object>> saveGames = obj.ToObject<List<Dictionary<string, object>>>();
+        int gameNo = 0;
+        Text saveGameText;
+        Text saveGameIDText;
+        foreach(Dictionary<string, object> saveGame in saveGames)
+        {
+            // create a gamebutton for each game here
+
+            if (gameNo == 0) {
+                SavedSessionButtonA.interactable = true;
+                saveGameText = SavedSessionButtonAText;
+                saveGameIDText = SavedSessionIDButtonAText;
+            } else if (gameNo == 1) {
+                SavedSessionButtonB.interactable = true;
+                saveGameText = SavedSessionButtonBText;
+                saveGameIDText = SavedSessionIDButtonBText;
+            } else {
+                saveGameText = null;
+                saveGameIDText = null;
+            }
+            
+            saveGameText.text = "SaveGame ID:\n\n";
+            saveGameIDText.text = (string)saveGame["savegameid"];
+            string players = saveGame["players"].ToString();
+            var charsToRemove = new string[] { "[", "]", "\n", "\"", " "};
+            foreach (var c in charsToRemove)
+            {
+                players = players.Replace(c, string.Empty);
+            }
+            saveGameText.text += "\nOriginal participants: " + players;
+            //fToken.text += "\nLaunch status: " + entry.Value["launched"].ToString();
+
+            // for now, we only have one button, so break after first session
+            gameNo++;   
+        }
+        if (gameNo==0) {
+            SavedSessionButtonAText.text = "";
+            SavedSessionIDButtonAText.text = "";
+            SavedSessionButtonA.interactable = false;
+            SavedSessionButtonBText.text = "";
+            SavedSessionButtonB.interactable = false;
+        } else if (gameNo==1) {
+            SavedSessionButtonBText.text = "";
+            SavedSessionIDButtonBText.text = "";
+            SavedSessionButtonB.interactable = false;
+        }
+    }
+
+    public void IntentToDelete() {
+        if(intentToDelete) {
+            intentToDelete = false;
+            deleteText.text = "Delete a saved game";
+        } else {
+            intentToDelete = true;
+            deleteText.text = "Go back";
+        }
+
+    }
+
+    public void DeleteSaveGame(Text savegameID) {
+        if(intentToDelete) {
+            string adminToken = GetAdminToken();
+            var request = new RestRequest("api/gameservices/ColtExpress/savegames/" + savegameID.text + "?access_token=" + adminToken, Method.DELETE)
+                .AddHeader("Authorization", "Basic YmdwLWNsaWVudC1uYW1lOmJncC1jbGllbnQtcHc=");
+            IRestResponse response = client.Execute(request);
+        }
     }
 
     private void LeaveSession() {
@@ -259,7 +404,7 @@ public class WaitingRoom : MonoBehaviour
         }
     }*/
 
-    /*private string GetAdminToken() {
+    private string GetAdminToken() {
         var request = new RestRequest("oauth/token", Method.POST)
             .AddParameter("grant_type", "password")
             .AddParameter("username", "admin")
@@ -270,7 +415,7 @@ public class WaitingRoom : MonoBehaviour
             var obj = JObject.Parse(response.Content);
             string adminToken = (string)obj["access_token"];
             return adminToken.Replace("+", "%2B");
-    }*/
+    }
 
     private void DeleteSession() {
         var request = new RestRequest("api/sessions/" + gameHash + "?access_token=" + token, Method.DELETE)
